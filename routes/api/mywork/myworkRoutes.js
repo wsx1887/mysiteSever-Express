d05895ad2c6ba0d5a40e0b8324f5e9ce76f9638c
wsx1887 <wsx1887@qq.com>
2019-08-02 01:48:38 +0800

    3

--- routes/api/mywork/myworkRoutes.js
+++ routes/api/mywork/myworkRoutes.js
@@ -1,6 +1,7 @@
 var router = require('express').Router();
 //var axios=require('axios');
-let emailTransporter=require('../../../common/emailTransporter.js')
+let emailTransporter=require('../../../common/emailTransporter.js');
+let sendEmailArray=require('./methods/sendEmailArray.js');
 
 var testlinkArray=require('./methods/testlink.js');
 
@@ -16,15 +17,22 @@ router.post('/testlink',async function(req, res, next) {
 router.post('/getimgurls',async function(req,res,next){
 
 });
-router.get('/sendemail',async function(req,res,next){
-    let message={
+router.post('/sendemail',function(req,res,next){
+    sendEmailArray(req.body).then(resArray=>{
+        //console.log(resArray);
+        res.status(200).send('发送成功');
+    }).catch(err=>{
+        console.log(err);
+        res.status(200).send(err);
+    });
+/*      let message={
         from:'wsx1887@qq.com',
         to:'547873304@qq.com',
         //subject:'cao',
         //text:'cao dong gua didi'
         html:`<img src="cid:0"/>`,
         attachments:[{
-            path:'D:/up/【纳兰性急作品】精致紧身黑裤MM [MP4 234MB] 188.mp4_20190731_150116.025.png',
+            path:'D:/up/【纳兰性急作品】精致紧身黑裤MM [MP4 234MB] 188.part1 (1).png',
             cid:'0'
         }]
     };
@@ -35,7 +43,7 @@ router.get('/sendemail',async function(req,res,next){
         }else{
             res.status(200).send('发送成功');
         }
-    });
+    });  */
 });
 
 module.exports = router;
