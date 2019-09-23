6cd7dc509fa5d4252105593a6945677958bafd9f
wsx1887 <wsx1887@qq.com>
2019-09-23 23:16:29 +0800

    1

--- public/myJS/jsForTampermonkey/取店名和日期.js
+++ public/myJS/jsForTampermonkey/取店名和日期.js
@@ -1,9 +1,17 @@
 (function(){
     var shopCart={
         getName(){
+            let nameString='';
             let alist=document.querySelectorAll('.seller-mod__name___37vL8');
+            alist.forEach(e=>nameString+=`${e.innerHTML}\n`);
+            return nameString;
         },
-        getDate(){}
+        getDate(){
+            let nameString='';
+            let alist=document.querySelectorAll('.bought-wrapper-mod__create-time___yNWVS');
+            alist.forEach(e=>nameString+=`${e.innerHTML}\n`);
+            return nameString;
+        }
     }
 
 })();
